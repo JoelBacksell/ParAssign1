b80179ba00020554646d50ab5ca75e9e7bab9559
JoelBacksell <joel.backsell@gmail.com>
2017-04-11 18:13:21 +0200

    finished Fox's algorithm

--- assign1.c
+++ assign1.c
@@ -3,21 +3,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 
 	int n = 4;
+
 	int rank, nproc, ndims, p1, p2;
 	int dims[2], coords[2], cyclic[2], reorder;
 	MPI_Comm proc_grid, proc_row, proc_col;
 
-	double *tempB,*tempA;
+	double *my_blockA, *my_blockB, *my_blockC, *tempA, *tempB;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	MPI_Request request;
+	MPI_Request request, send_request, recv_request;
 
 	MPI_Status status;
 
@@ -39,31 +41,33 @@ int main(int argc, char *argv[]) {
 	MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, cyclic, reorder, &proc_grid);
 	MPI_Cart_coords(proc_grid, rank, ndims, coords);
 
+	int my_rank;
+	MPI_Cart_rank(proc_grid, coords, &my_rank);
+
 	//printf("%4d %4d %4d\n",coords[0], coords[1], rank);
 
-	MPI_Comm_split(proc_grid, coords[0], coords[1], &proc_row);
-	MPI_Comm_split(proc_grid, coords[1], coords[0], &proc_col);
+	int row_rank, column_rank, row_size, column_size;
 
-	//MPI_Datatype vec_type;
+	// Create a communicator for each row
+	MPI_Comm_split(proc_grid, coords[0], coords[1], &proc_row);
+  	MPI_Comm_rank(proc_row, &row_rank);
+  	MPI_Comm_size(proc_row, &row_size);
+
+
+  	// Create a communicator for each column
+	MPI_Comm_split(proc_grid, coords[1], coords[0], &proc_col);
+  	MPI_Comm_rank(proc_col, &column_rank);
+    MPI_Comm_size(proc_col, &column_size);
+
+    //printf("My row rank: %d\n", row_rank);
+
 	MPI_Type_vector(r1, r2, n, MPI_DOUBLE, &vec_type);
 	MPI_Type_commit(&vec_type);
 
-
-	int row_rank,column_rank,row_size,column_size;
-	 // Create a communicator for each row
-  	
-  	MPI_Comm_rank(proc_row,&row_rank);
-  	MPI_Comm_size(proc_row,&row_size);
-
-  	// Create a communicator for each column
-  	
-  	MPI_Comm_rank(proc_col,&column_rank);
-    MPI_Comm_size(proc_col,&column_size);
-
-	
+	double *matrixC;
 
 	if (rank == 0) {
-		double *matrixA, *matrixB,* matrixC;
+		double *matrixA, *matrixB;//, *matrixC;
 		matrixA = malloc(n * n * sizeof(double));
 		matrixB = malloc(n * n * sizeof(double));
 		matrixC = malloc(n * n * sizeof(double));
@@ -79,28 +83,31 @@ int main(int argc, char *argv[]) {
 			matrixB[i] = i + n * n;
 		}
 
-		// printf("Matrix A:\n");
-		// for (i = 0; i < n * n; i++) {
-		// 	printf("%f, ", matrixA[i]);
-		// }
-		// printf("Matrix B:\n");
-		// for (i = 0; i < n * n; i++) {
-		// 	printf("%f, ", matrixB[i]);
-		// }
+		printf("Matrix A:\n");
+		for (i = 0; i < n * n; i++) {
+			printf("%f, ", matrixA[i]);
+		}
+		printf("\n");
+		printf("Matrix B:\n");
+		for (i = 0; i < n * n; i++) {
+			printf("%f, ", matrixB[i]);
+		}
+		printf("\n");
 
 
 		for (i = 0; i < p1; i++) {
 			for (j = 0; j < p2; j++) {
 
-				printf("Top corner: %4f\n", matrixA[i * p1 + j * n * p2]);
+				// printf("Top corner A: %4f\n", matrixA[i * p1 + j * n * p2]);
+				// printf("Top corner B: %4f\n", matrixB[i * p1 + j * n * p2]);
 
 				int grid_rank;
 				int loc[2] = {i, j};
 				MPI_Cart_rank(proc_grid, loc, &grid_rank);
-				MPI_Isend(&matrixA[i * p1 + j * n * p2], 1, vec_type, grid_rank, grid_rank, proc_grid, &request);
-				MPI_Wait(&request, &status);
-				MPI_Isend(&matrixB[i * p1 + j * n * p2], 1, vec_type, grid_rank, grid_rank, proc_grid, &request);
-				MPI_Wait(&request, &status);
+				MPI_Isend(&matrixA[i * r1 * n + j * r2], 1, vec_type, grid_rank, grid_rank, proc_grid, &send_request);
+				MPI_Wait(&send_request, &status);
+				MPI_Isend(&matrixB[i * r1 * n + j * r2], 1, vec_type, grid_rank, grid_rank, proc_grid, &send_request);
+				MPI_Wait(&send_request, &status);
 			}
 		}
 
@@ -112,43 +119,85 @@ int main(int argc, char *argv[]) {
 
 	}
 
-	double *my_blockA, *my_blockB,*my_blockC;
+	
 	my_blockA = malloc(r1 * r2 * sizeof(double));
 	my_blockB = malloc(r1 * r2 * sizeof(double));
-	my_blockC = malloc(r1 * r2 * sizeof(double));
+	my_blockC = calloc(r1 * r2, sizeof(double));
 
-	int my_rank;
-	MPI_Cart_rank(proc_grid, coords, &my_rank);
 
 	MPI_Recv(my_blockA, r1 * r2, MPI_DOUBLE, 0, my_rank, proc_grid, &status);
 	MPI_Recv(my_blockB, r1 * r2, MPI_DOUBLE, 0, my_rank, proc_grid, &status);
 
 	
-	printf("A: %4f\n", my_blockA[0]);
-	printf("B: %4f\n", my_blockB[0]);
+	printf("A: %4f, %d\n", my_blockA[0], my_rank);
+	printf("B: %4f, %d\n", my_blockB[0], my_rank);
 
 
+	tempA = malloc(r1 * r2 * sizeof(double));
+	//tempB = malloc(r1 * r2 * sizeof(double));
 
-	tempA=(double*)calloc(r1*r1,sizeof(double));
-	tempB=(double*)calloc(r1*r1,sizeof(double));
 
-
-	int k;
+	int k, row, col, l;
 	for (k = 0; k < p1; k++) {
 		int m = (coords[0] + k) % p1;
-		if (row_rank==m){
+		MPI_Isend(my_blockB, r1 * r2, MPI_DOUBLE, (p1 + column_rank - 1) % p1, column_rank, proc_col, &send_request);
+		if (row_rank == m) {
 			MPI_Bcast(my_blockA, r1 * r2, MPI_DOUBLE, m, proc_row);
+			for (row = 0; row < r1; row++) {
+				for (col = 0; col < r2; col++) {
+					for (l = 0; l < r2; l++) {
+						my_blockC[row * r2 + col] += my_blockA[row * r2 + l] * my_blockB[l * r2 + col];
+					}
+				}
+			}
+		} else {
+			MPI_Bcast(tempA, r1 * r2, MPI_DOUBLE, m, proc_row);
+			for (row = 0; row < r1; row++) {
+				for (col = 0; col < r2; col++) {
+					for (l = 0; l < r2; l++) {
+						my_blockC[row * r2 + col] += tempA[row * r2 + l] * my_blockB[l * r2 + col];
+					}
+				}
+			}
 		}
-		else {
-			MPI_Bcast(tempB, r1 * r2, MPI_DOUBLE, m, proc_row);
-			
 
-		}
-		
+		//MPI_Isend(my_blockB, r1 * r2, MPI_DOUBLE, (p1 + column_rank - 1) % p1, column_rank, proc_col, &send_request);
+		MPI_Irecv(my_blockB, r1 * r2, MPI_DOUBLE, (column_rank + 1) % p1, (column_rank + 1) % p1, proc_col, &recv_request);
+		MPI_Wait(&send_request, &status);
+		MPI_Wait(&recv_request, &status);
+		//memcpy(my_blockB, tempB, r1 * r2 * sizeof(double));
 	}
+
+	printf("C: %4f, %d\n", my_blockC[0], my_rank);
+
+	MPI_Isend(my_blockC, r1 * r2, MPI_DOUBLE, 0, my_rank, proc_grid, &request);
+
+	if (rank == 0) {
+		int i;
+		for (i = 0; i < nproc; i++) {
+			MPI_Probe(i, i, proc_grid, &status);
+			int loc[2];
+			MPI_Cart_coords(proc_grid, i, ndims, loc);
+			MPI_Recv(&matrixC[loc[0] * r1 * n + loc[1] * r2], 1, vec_type, i, i, proc_grid, &status);
+		}
+	}
+
+	MPI_Wait(&request, &status);
+
+	if (rank == 0) {
+		int i;
+		printf("Matrix C:\n");
+		for (i = 0; i < n * n; i++) {
+			printf("%f, ", matrixC[i]);
+		}
+		printf("\n");
+	}
+	
 
 	free(my_blockA);
 	free(my_blockB);
+	free(my_blockC);
+	free(tempA);
 
 	MPI_Finalize();
 
